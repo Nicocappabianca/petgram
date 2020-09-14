0d8817e335a6a331b970a61c00e20ebdd2e3d9eb
nicocappabianca <nicolas.cappabianca@gmail.com>
2020-09-14 20:16:27 -0300

    add container listOfPhotoCards

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { withPhotos } from '../../hoc/withPhotos'
 
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
     return(
         <ul>
             {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
         </ul>
     )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
